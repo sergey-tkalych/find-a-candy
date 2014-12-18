2a0ed3122e3c78f7ea80d9005f567bdd34f82822
Sergey Tkalych <set@qapint.com>
2014-12-18 22:31:05 +0200

    update(gulp): live reload

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ var uglify = require('gulp-uglify');
 var minifyHtml = require('gulp-minify-html');
 var minifyCss = require('gulp-minify-css');
 var rev = require('gulp-rev');
+var connect = require('gulp-connect');
+var watch = require('gulp-watch');
+var path = require('path');
 
 gulp.task('usemin', function () {
 	return gulp.src('./index.html')
@@ -21,6 +24,19 @@ gulp.task('images', function(){
 });
 
 gulp.task('build', ['usemin', 'images'], function(){});
+
+gulp.task('live', ['connect'], function() {
+	return watch(['css/*.css', 'js/*.js', 'index.html'])
+		.pipe(connect.reload());
+});
+
+gulp.task('connect', function() {
+	connect.server({
+		livereload: true,
+		port: 8000,
+		root: path.resolve('./')
+	});
+});
 
 gulp.task('default', function(){
 	console.log('Hello from Gulp');
